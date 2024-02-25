6e26545c05e758a9a1ac603ca405b3f4b8fc435a
Nirilala Rakotondrasoa <nirilala15@gmail.com>
2024-02-25 13:38:19 +0100

    feat: add basics commands

--- celerity-cli/src/main.rs
+++ celerity-cli/src/main.rs
@@ -1,18 +1,32 @@
+use core::projects::template::list;
 use clap::Parser;
-
+use comfy_table::Table;
 
 mod commands;
+mod texts;
 
 #[tokio::main]
 async fn main(){
     let cmd = commands::Commands::parse();
+    let mut table = Table::new();
 
     match cmd {
         commands::Commands {template_list: true, ..} => {
-
+            let templates = list(String::from("examples/templates"));
+            let template_length = templates.len();
+            table.set_header(vec!["Name", "Author"]);
+            for template in templates {
+                if let Some(template) = template {
+                    table.add_row(vec![
+                        template.name,
+                        template.author
+                    ]);
+                }
+            }
+            println!("Templates list ({})\n{}", template_length, table)
         },
         _ => {
-            println!("Hello world")
+            println!("{}", texts::lib_description());
         }
     }
 }
--- core/src/projects/template.rs
+++ core/src/projects/template.rs
@@ -1,7 +1,11 @@
+use std::fs;
+
 use serde::{Deserialize, Serialize};
 
+use crate::utils::json::read_json;
+
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Path {
+pub struct TemplatePath {
     pub source: String,
     pub uri: String,
     pub branch: Option<String>
@@ -11,6 +15,22 @@ pub struct Path {
 pub struct Template {
     pub name: String,
     pub author: String,
-    pub path: Path,
+    pub path: TemplatePath,
     pub description: String
+}
+
+pub fn list(folder: String) -> Vec<Option<Template>>{
+    match fs::read_dir( std::path::Path::new(&folder)) {
+        Ok(dirs) => {
+            let res = dirs.map::<Option<Template>, _>(|dir| {
+                if let Ok(dir) = dir {
+                    return read_json::<Template>(dir.path().to_string_lossy().to_string());
+                }
+                Option::None
+            });
+            return res.collect::<Vec<Option<Template>>>();
+        },
+        _ => {}
+    }
+    vec![]
 }
