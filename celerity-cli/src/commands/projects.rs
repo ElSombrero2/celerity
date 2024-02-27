b5897f5989f327f9a4c34569c23aac4b38add9d6
Nirilala Rakotondrasoa <nirilala15@gmail.com>
2024-02-27 14:48:27 +0100

    feat: add refresh token and beautify cli feedback

--- celerity-cli/src/commands/projects.rs
+++ celerity-cli/src/commands/projects.rs
@@ -1,10 +1,18 @@
 use core::{git::{clone_project, reinit}, projects::types::Template, utils::json::read_json};
 use super::types::project::Project;
-use ansi_term::Colour::{Red, Green};
+use ansi_term::Colour::{Red, Green, Blue, White};
+use spinoff::{spinners, Color, Spinner};
+
+fn show_message(message: String, spinner: &mut Spinner){
+    println!();
+    spinner.update_text(message);
+}
 
 pub fn init_project(init: Project){
+    let mut sp = Spinner::new(spinners::Dots8, "Initialize your project", Color::White);
     match init {
         Project::Init { name, template, path } => {
+            show_message(format!("🔎 Searching for template {}", Blue.bold().paint(&template)), &mut sp);
             let filepath = "./examples/templates/".to_owned() + template.as_str() + ".json";
             if let Some(template) = read_json::<Template>(filepath) {
                 if let Some (dir) =  clone_project(
@@ -12,15 +20,20 @@ pub fn init_project(init: Project){
                     template.path.branch,
                     path.to_owned() + (if path.ends_with('/') {""} else { "/" }) + name.as_str()
                 ) {
+                    show_message("📝 Init and commit your project, almost done!".to_owned(), &mut sp);
                     let to_remove = dir.to_owned();
                     let to_init = dir.replace(".git/", "");
                     if reinit(to_remove, to_init.to_owned()) {
-                        println!("Your project was {} init at {}", Green.bold().paint("successfully"), to_init);
+                        sp.stop_with_message(
+                            &format!("Your project was {} init at {}", 
+                            Green.bold().paint("successfully"),
+                            White.bold().paint(to_init)
+                        ));
                         return;
                     }
                 }
             }
-            println!("An {} occured while initialize your project", Red.bold().paint("error"));
+            sp.stop_with_message(&format!("An {} occured while initialize your project", Red.bold().paint("error")));
         }
     }
 }
--- celerity-cli/src/main.rs
+++ celerity-cli/src/main.rs
@@ -1,8 +1,8 @@
-use core::{config::types::Configuration, git::github::server::start_server, utils::json::read_json};
+use core::{config::types::Configuration, git::github::{ping, server::start_server}, utils::json::read_json};
 use clap::Parser;
 use comfy_table::Table;
 use commands::{projects::init_project, templates::show_template_list};
-use texts::{config_error, lib_description};
+use texts::{config_error, expiration_message, lib_description};
 
 mod commands;
 mod texts;
@@ -12,9 +12,13 @@ async fn main(){
     dotenvy::dotenv().expect("Error while loading env");
     let cmd = commands::Commands::parse();
     let mut table = Table::new();
-    let config = read_json::<Configuration>(".config/configuration.json".to_owned());
     if cmd.github_login { start_server().await; }
-    if config.is_some() {
+    if let Some(config) = read_json::<Configuration>(".config/configuration.json".to_owned()) {
+        if !ping(config.github_token.unwrap_or_default()){
+            expiration_message();
+            start_server().await;
+            println!("Reexecute your command");
+        }
         match cmd {
             commands::Commands {templates: true, ..} => show_template_list(&mut table),
             commands::Commands {init: Some(init), ..} => init_project(init),
