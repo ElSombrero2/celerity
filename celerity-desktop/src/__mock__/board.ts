73f88fe5c8f90c7aee4c44f244e6c4ad31d67716
Nirilala Rakotondrasoa <nirilala15@gmail.com>
2024-03-05 23:13:22 +0100

    feat: add mock data

--- celerity-desktop/src/__mock__/board.ts
+++ celerity-desktop/src/__mock__/board.ts
@@ -2,39 +2,35 @@ export const board = {
     "Backlog": [
         {
             "id": "29c5d7e3495044779226dbf0ae5cf786",
-            "title": "Create magic truc 1"
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
             "id": "29c5d7e3495044779226dbf0ae5cf786",
-            "title": "Create magic truc 2"
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
         },
         {
             "id": "29c5d7e3495044779226dbf0ae5cf786",
-            "title": "Create magic truc 3"
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,"
         }
     ],
     "Work in progress": [
         {
-            "id": "e0042b5c92c34229b1aa8629535e0e59",
-            "title": "Initialize the project 1"
+            "id": "29c5d7e3495044779226dbf0ae5cf786",
+            "title": "Lorem ipsum dolor sit amet,"
         },
         {
-            "id": "e0042b5c92c34229b1aa8629535e0e59",
-            "title": "Initialize the project 2"
+            "id": "29c5d7e3495044779226dbf0ae5cf786",
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
         },
         {
-            "id": "e0042b5c92c34229b1aa8629535e0e59",
-            "title": "Initialize the project 3"
-        },
-        {
-            "id": "e0042b5c92c34229b1aa8629535e0e59",
-            "title": "Initialize the project 4"
+            "id": "29c5d7e3495044779226dbf0ae5cf786",
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
         }
     ],
     "Finished": [
         {
             "id": "d17afe1e9942490fa8bb8cbd047b0dd7",
-            "title": "Cook"
+            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         }
     ]
 }
